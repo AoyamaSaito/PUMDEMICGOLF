69e65d33856fb4c0574fd696f2f429849749d1aa
aoyama_saito <saitomarimo1229@gmail.com>
2021-08-25 17:18:32 +0900

    clearシステムを追加

---
+++ Assets/script/StageIn.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Cinemachine;
+
+public class StageIn : MonoBehaviour
+{
+    [SerializeField] CinemachineVirtualCameraBase m_zombiCam = default;
+    [SerializeField] CinemachineVirtualCameraBase m_billCam = default;
+    [SerializeField] string loadScene;
+    Animator FadeAnim;
+    void Start()
+    {
+        GameObject gameObject = GameObject.Find("Fade");
+        FadeAnim = gameObject.GetComponent<Animator>();
+        SwitchZombiCam();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "StageIn")
+        {
+            Debug.Log("StageIn");
+            m_billCam.Priority = 11;
+            StartCoroutine(InStage());
+        }
+    }
+
+    private void SwitchZombiCam()
+    {
+        if (m_zombiCam)
+        {
+            m_zombiCam.Priority = 10;
+        }
+    }
+
+    IEnumerator InStage()
+    {
+        yield return new WaitForSeconds(4f);
+        FadeAnim.Play("FadeAnim");
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(loadScene);
+    }
+
+}
--- Assets/script/siminclear.cs
+++ Assets/script/siminclear.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class siminclear : MonoBehaviour
 {
     Animator FadeAnim;
+    Animator Elevator1;
+    Animator Elevator2;
     [SerializeField] GameObject gameObject1;
+    GameObject gameObject3;
     // Start is called before the first frame update
     void Start()
     {
         GameObject gameObject = GameObject.Find("Fade");
         FadeAnim = gameObject.GetComponent<Animator>();
+        GameObject gameObject1 = GameObject.Find("Deguchi1");
+        Elevator1 = gameObject1.GetComponent<Animator>();
+        GameObject gameObject2 = GameObject.Find("Deguchi2");
+        Elevator2 = gameObject2.GetComponent<Animator>();
+        gameObject3 = GameObject.Find("hijoguchi");
+        gameObject3.SetActive(false);
     }
 
     // Update is called once per frame
@@ -34,5 +43,8 @@ public class siminclear : MonoBehaviour
         yield return new WaitForSeconds(1f);
         Instantiate(gameObject1, this.transform.position, Quaternion.identity);
         Destroy(this.gameObject);
+        Elevator1.Play("deguchiElevator");
+        Elevator2.Play("deguchiElevator2");
+        gameObject3.SetActive(true);
     }
 }
